8f1f138dc6fb0d5314f2bbb7426cc3ddc2173c99
Alvaro Martinez <alvaro.martinez@unosquare.com>
2022-07-15 12:14:50 -0500

    inital commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  function fetchMoviesHandler() {
+    setIsLoading(true);
+    fetch('https://swapi.dev/api/films/')
+      .then((res) => res.json())
+      .then((data) => {
+        const transformedMovies = data.results.map((movie) => {
+          return {
+            id: movie.episode_id,
+            title: movie.title,
+            openingText: movie.opening_crawl,
+            releaseDate: movie.release_date
+          };
+        });
+        setMovies(transformedMovies);
+        setIsLoading(false);
+      });
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>not found movies</p>}
+        {isLoading && <p>...loading</p>}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
